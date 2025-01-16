3e8c19d3a827b7b435a98d6821199565a121ec47
Seamor Estrabon <seamorestrabon@gmail.com>
2025-01-16 15:01:08 +0800

    ✨ feat: Add form with first name label and input field

--- src/components/Editor/Personal.jsx
+++ src/components/Editor/Personal.jsx
@@ -3,6 +3,23 @@ export default function Personal() {
     <>
       <h3>Personal Details</h3>
       <hr className="grey-line" />
+      <form className="personl-details">
+        <div className="fName">
+          <label htmlFor="fName" data-help="you need help?">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="fName"
+            id="fName"
+            pattern="^[^0-9]+$"
+            size="15"
+            maxLength="25"
+            placeholder="First Name"
+            required
+          />
+        </div>
+      </form>
     </>
   );
 }
