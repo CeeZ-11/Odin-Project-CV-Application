dd0234f6814e3a0b0258cd892bd35a2630a5ee2d
Seamor Estrabon <seamorestrabon@gmail.com>
2025-01-16 18:17:46 +0800

    Update changes to fix gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/https://github.com/CeeZ-11/Odin-Project-CV-Application/",
   server: {
     watch: {
       usePolling: true, // Force polling to detect changes
