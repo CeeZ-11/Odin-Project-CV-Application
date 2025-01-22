aa90ccc9a134a805321104f58832713b3a9a3f76
Seamor Estrabon <seamorestrabon@gmail.com>
2025-01-22 19:17:36 +0800

    :sparkles: feat: Add DisplayExperience component and import to Experience component

---
+++ src/components/Editor/DisplayExperience.jsx
@@ -0,0 +1,60 @@
+export default function DisplayExperience({
+  id,
+  job_title,
+  employer,
+  employment_type,
+  date,
+  end_date,
+  location,
+  work_arrangement,
+  description,
+  display,
+  removeExperience,
+  changeExperienceDisplay,
+  handleExpInputChange,
+  changeExperienceFormDisplay,
+}) {
+  return (
+    <>
+      {display && (
+        <div className="experience-list">
+          <h5>
+            {job_title} at {employer}
+          </h5>
+          <p>
+            {date} - {end_date}
+          </p>
+        </div>
+      )}
+
+      {!display && (
+        <DisplayExperienceForm
+          id={id}
+          job_title={job_title}
+          employer={employer}
+          employment_type={employment_type}
+          date={date}
+          end_date={end_date}
+          location={location}
+          work_arrangement={work_arrangement}
+          description={description}
+          handleExpInputChange={handleExpInputChange}
+          changeExperienceFormDisplay={changeExperienceFormDisplay}
+        />
+      )}
+
+      <div className="experience-btn-div">
+        <button
+          type="button"
+          onClick={() => changeExperienceDisplay(id)}
+          className="experience-btn"
+        >
+          Edit
+        </button>
+        <button type="button" onClick={() => removeExperience(id)}>
+          x
+        </button>
+      </div>
+    </>
+  );
+}
--- src/components/Editor/Experience.jsx
+++ src/components/Editor/Experience.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import DisplayExperience from "./DisplayExperience";
 
 const experienceList = [
   {
