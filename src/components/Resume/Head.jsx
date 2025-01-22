fad4a017187cb19612796e097cadf4b1427a7c69
Seamor Estrabon <seamorestrabon@gmail.com>
2025-01-23 03:31:48 +0800

    :sparkles: feat: Add head details in Head component
    :art: style: Update layout of Head component and some adjustments

--- src/components/Resume/Head.jsx
+++ src/components/Resume/Head.jsx
@@ -7,7 +7,22 @@ export default function Head() {
         <button className="profile-button"></button>
       </div>
 
-      <div className="head-profile-header"></div>
+      <div className="head-profile-header">
+        <h2>Jordan Clark</h2>
+        <h4>Graphic Designer</h4>
+        <div className="head-details">
+          <div className="head-phone"></div>
+          <p>(+54) 9 2706 64116</p>
+        </div>
+        <div className="head-details">
+          <div className="head-email"></div>
+          <p>seamorestrabon@gmail.com</p>
+        </div>
+        <div className="head-details">
+          <div className="head-website"></div>
+          <p>ceez-11.github.io/Portfolio_Website/</p>
+        </div>
+      </div>
     </div>
   );
 }
